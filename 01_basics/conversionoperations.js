28e8ca77a60a567c9cb9a493857682bba2bfe34d
Piyush Kumar <piyuxhkr1312@gmail.com>
2026-04-09 15:31:03 +0000

    Conversion of datatypes

---
+++ 01_basics/conversionoperations.js
@@ -0,0 +1,37 @@
+let score1 = "33abc"
+let score2 = 33
+let score3 = null
+score4 = true
+console.log(typeof (score1) )
+console.log(typeof score2);
+console.log(typeof score3);
+
+let valueInNumber = Number(score1)
+let valueInNumber2 = Number(score3)  
+console.log(typeof valueInNumber);
+/*NaN is short for "Not-a-Number". 
+In JavaScript, NaN is a number that is not a legal number.
+ The Global NaN property is the same as the Number. Nan property. */
+
+
+console.log(typeof valueInNumber2);
+console.log(valueInNumber);
+
+console.log(typeof score4)
+
+//"33" => 33
+//"33abc" => NaN
+// true=> 1, false => 0
+
+let isoggenIn = 1
+let booleaanIsLoggedIn = Bollean(isLoggedIn)
+console.log(booleaanIsLoggedIn)
+
+// 1 => true , 0 => false
+// "" => false
+// "piyush" => true
+
+let someNumber = 33
+ let stringNumber = string(someNumber)
+ console.log(stringNumber) 
+ console.log(typeof stringNumber)  
