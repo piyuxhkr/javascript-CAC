4294a59c6753934d7741b40dc95e4d38c7f53b28
Piyush Kumar <piyuxhkr1312@gmail.com>
2026-04-09 14:14:04 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; //treat all JS code as newer and modern version
+
+//alert(3+3)//we are using node js not browser so alert will not work
+ console.log(3+3) 
+ console.log("Piyush") 
+ 
+let name = "Piyush"
+let age = 18
+let isLoggedIn = false
+
+
+//Data types in JS
+//PRIMITIVE DATA TYPES
+
+// number => 2 to power 53
+// bigint => 
+// string => "Piyush"
+// boolean => true/false
+// null => standalone value that represents nothingness or empty value
+// undefined => variable that is not assigned a value yet
+// symbol => unique identifier
+
+
+// OBJECT
+
+console.log(typeof null) //object // we use typeof to know daTA TYPE of any variable
+// typeof null is object but it is not an object it is a primitive data type
+// it is a bug in JS but we cannot change it because of backward compatibility
